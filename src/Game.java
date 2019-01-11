8fda847bdd10039c3881ee0a1cadc3c4f4f1c698
malcolm-smith <malcolm.henry.smith@gmail.com>
2019-01-11 09:03:54 -0500

    Created frame

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,21 @@
+import javax.swing.JFrame;
+
 public class Game {
 
+    private JFrame frame = new JFrame();
+
+    public Game() {
+        this.initGUI();
+    }
+
+    private void initGUI() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("hey");
+        Game g = new Game();
     }
 }
